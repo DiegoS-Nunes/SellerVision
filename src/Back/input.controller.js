e0cf239b437f82a9008d019efb6d77b2a4f2ed5b
Diego Nunes <diego.nunes@fatec.sp.gov.br>
2024-07-06 22:43:41 -0300

    removendo script.js e tratando mudanças de estado dos input com react

--- src/Back/input.controller.js
+++ src/Back/input.controller.js
@@ -1,32 +1,20 @@
-const Inome = document.querySelector(".nome");
-const Iemail = document.querySelector(".email");
-const Itel = document.querySelector(".tel");
-const Isenha = document.querySelector(".senha");
-
-function cadastrar () {
-    fetch("http://localhost:5173/cadastro",
-        {
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-            },
-            method: "POST",
-            body: JSON.stringify({
-                nome: Inome.value,
-                email: Iemail.value,
-                tel: Itel.value,
-                senha: Isenha.value
-            })
-        })
-        .then(function (res) {console.log(res)})
-        .catch(function (res) {console.log(res)})
-}
-
-function limpar (){
-    Inome.value = "";
-    Iemail.value = "";
-    Itel.value = "";
-    Isenha.value = "";
-}
-
-export default {cadastrar, limpar}
+export function cadastrar({ nome, username, email, telefone, senha }) {
+    fetch("http://localhost:8080/usuarios", {
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ nome, username, email, telefone, senha })
+    })
+    .then(function (res) { console.log(res); })
+    .catch(function (res) { console.log(res); });
+  }
+  
+  export function limpar(setNome, setUsername, setEmail, setTelefone, setSenha) {
+    setNome("");
+    setUsername("");
+    setEmail("");
+    setTelefone("");
+    setSenha("");
+  }
--- src/Front/App.jsx
+++ src/Front/App.jsx
@@ -1,22 +1,35 @@
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import { cadastrar, limpar } from '../back/input.controller.js';
 
 function App() {
+  const [nome, setNome] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    cadastrar({ nome, username, email, telefone, senha });
+    limpar(setNome, setUsername, setEmail, setTelefone, setSenha);
+  };
 
   return (
     <>
       <div className="box">
-        <form action="">
-            <h1>Cadastro</h1>
-            <input type="text" className="nome" name="nome" id="nome" autoComplete="on" placeholder="Nome"/>
-            <input type="text" className="username" name="username" id="username" autoComplete="off" placeholder="Nome de Usuário"/>            
-            <input type="email" className="email" name="email" id="email" autoComplete="on" placeholder="Email"/>
-            <input type="tel" className="tel" name="telefone" id="telefone" autoComplete="on" placeholder="Telefone"/>
-            <input type="password" className="senha" name="senha" id="senha" autoComplete="on" placeholder="Senha"/>
-            <button>Cadastrar</button>
+        <form onSubmit={handleSubmit}>
+          <h1>Cadastro</h1>
+          <input type="text" className="nome" value={nome} onChange={(e) => setNome(e.target.value)} autoComplete="on" placeholder="Nome"/>
+          <input type="text" className="username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete="off" placeholder="Nome de Usuário"/>
+          <input type="email" className="email" value={email} onChange={(e) => setEmail(e.target.value)} autoComplete="on" placeholder="Email"/>
+          <input type="telefone" className="telefone" value={telefone} onChange={(e) => setTelefone(e.target.value)} autoComplete="on" placeholder="Telefone"/>
+          <input type="password" className="senha" value={senha} onChange={(e) => setSenha(e.target.value)} autoComplete="on" placeholder="Senha"/>
+          <button type="submit">Cadastrar</button>
         </form>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
